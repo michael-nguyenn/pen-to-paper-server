3927b4ccc6ba80fc9c8d6fbeff4fc0abce3ad571
Michael Nguyen <n93michael@gmail.com>
2022-06-22 21:10:52 -0400

    endpoints created for entries and entries by id

--- controllers/entriesController.js
+++ controllers/entriesController.js
@@ -9,3 +9,62 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Entries: ${err}`);
     });
 };
+
+exports.singleEntry = (req, res) => {
+  knex("entries")
+    .where({ id: req.params.id })
+    .then((data) => {
+      if (!data.length) {
+        return res
+          .status(404)
+          .send(`Record with id: ${req.params.id} is not found`);
+      }
+
+      res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
+    );
+};
+
+exports.addEntry = (req, res) => {
+  !req.body.entry
+    ? res.status(400).send("Please fill out the entry")
+    : knex("entries")
+        .insert(req.body)
+        .then((data) => {
+          const newEntryURL = `/entries/${data[0]}`;
+          res.status(201).location(newEntryURL).send(newEntryURL);
+        })
+        .catch((err) => res.status(400).send(`Error creating Entry: ${err}`));
+};
+
+exports.deleteEntry = (req, res) => {
+  knex("entries")
+    .delete()
+    .where({ id: req.params.id })
+    .then(() => {
+      res.status(204).send(`Entry with id: ${req.params.id} has been deleted`);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error deleting Entry ${req.params.id} ${err}`)
+    );
+};
+
+exports.updateEntry = (req, res) => {
+  !req.body.entry
+    ? res.status(400).send("Please fill out the entry")
+    : knex("entries")
+        .update(req.body)
+        .where({ id: req.params.id })
+        .then(() => {
+          res
+            .status(200)
+            .send(`Warehouse with id: ${req.params.id} has been updated`);
+        })
+        .catch((err) =>
+          res
+            .status(400)
+            .send(`Error updated Warehouse ${req.params.id} ${err}`)
+        );
+};
--- routes/entriesRoute.js
+++ routes/entriesRoute.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const entriesController = require("../controllers/entriesController");
 
-router.route("/").get(entriesController.index);
+router
+  .route("/")
+  .get(entriesController.index)
+
+  .post(entriesController.addEntry);
+
+router
+  .route("/:id")
+  .get(entriesController.singleEntry)
+  .delete(entriesController.deleteEntry)
+  .patch(entriesController.updateEntry);
 
 module.exports = router;
