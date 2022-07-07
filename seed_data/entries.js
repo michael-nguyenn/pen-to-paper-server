c5a91eca82e105b44bda7f09df95ff4beb91e498
Michael Nguyen <n93michael@gmail.com>
2022-07-07 09:59:09 -0400

    added seed data

--- seed_data/entries.js
+++ seed_data/entries.js
@@ -1,14 +1,8 @@
 module.exports = [
   {
     id: 1,
-    content: "Today I went to the beach. It was nice",
-    type: "Daily Entry",
-    is_template: false,
-  },
-  {
-    id: 2,
-    content: "Hello world. What a wonderful day",
-    type: "Daily Entry",
-    is_template: true,
+    title: "Showerthoughts",
+    content:
+      '{"blocks": [{"key": "1v6kd", "data": {}, "text": "When random thoughts pop up, journal it!", "type": "unstyled", "depth": 0, "entityRanges": [], "inlineStyleRanges": [{"style": "fontsize-24", "length": 40, "offset": 0}, {"style": "BOLD", "length": 40, "offset": 0}, {"style": "color-rgb(41,105,176)", "length": 40, "offset": 0}]}, {"key": "spco", "data": {}, "text": "Security at every level of an airport is absolutely ridiculous. Until you get to the baggage claim. Then it\'s just like take whatever bag you want.", "type": "ordered-list-item", "depth": 0, "entityRanges": [], "inlineStyleRanges": [{"style": "fontsize-14", "length": 147, "offset": 0}]}, {"key": "dp4d5", "data": {}, "text": "I just spent 40 minutes in the shower thinking why there\'s a thing called a grapefruit, when there\'s a fruit called grape", "type": "ordered-list-item", "depth": 0, "entityRanges": [], "inlineStyleRanges": [{"style": "fontsize-14", "length": 121, "offset": 0}]}, {"key": "4esv3", "data": {}, "text": "The biggest flex is using mobile data in your own home because the WiFi is being slow...", "type": "ordered-list-item", "depth": 0, "entityRanges": [], "inlineStyleRanges": [{"style": "fontsize-14", "length": 88, "offset": 0}]}], "entityMap": {}}',
   },
 ];
--- seeds/entries_templates.js
+++ seeds/entries_templates.js
@@ -1,9 +1,16 @@
 const templateData = require("../seed_data/templates");
+const entriesData = require("../seed_data/entries");
 
 exports.seed = function (knex) {
   return knex("templates")
     .del()
     .then(function () {
       return knex("templates").insert(templateData);
+    })
+    .then(() => {
+      return knex("entries").del();
+    })
+    .then(function () {
+      return knex("entries").insert(entriesData);
     });
 };
